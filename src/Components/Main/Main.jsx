99db62de3300711d23ff72e7ba155b0e4f6fecc2
Md Econozzaman Econ <92664558+ezecon@users.noreply.github.com>
2025-06-21 15:34:39 +0600

    first commit

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -3,28 +3,30 @@ import React from 'react';
 import Draggable from 'react-draggable';
 
 const images = [
-  'https://via.placeholder.com/150?text=Image+1',
-  'https://via.placeholder.com/150?text=Image+2',
-  'https://via.placeholder.com/150?text=Image+3',
-  'https://via.placeholder.com/150?text=Image+4',
-  'https://via.placeholder.com/150?text=Image+5',
-  'https://via.placeholder.com/150?text=Image+6',
-  'https://via.placeholder.com/150?text=Image+7',
-  'https://via.placeholder.com/150?text=Image+8',
+  '1.jpg',
+  '2.jpg',
+  '3.jpg',
+  '4.jpg',
+  '5.jpg',
+  '6.jpg',
+  '7.jpg',
+  '8.jpg',
 ];
 
 const Main = () => {
   return (
-    <div className="relative w-screen h-screen bg-gray-100 overflow-hidden">
+    <div className="flex justify-center items-center relative w-screen h-screen bg-gray-100 overflow-hidden">
+      <p className='playwrite-cu-caption'>Aab Mai Keya Karoon</p>
       {images.map((src, index) => (
         <Draggable key={index}>
-          <div className="absolute cursor-move">
+          <div className="absolute cursor-move ">
             <img
               src={src}
               alt={`Image ${index + 1}`}
-              className="w-36 h-36 object-cover border border-gray-400 shadow-md rounded"
+              className="w-72 h-72 object-cover border border-gray-400 shadow-md rounded"
             />
           </div>
+          
         </Draggable>
       ))}
     </div>
